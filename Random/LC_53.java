bdc6a276879a2d6a73f2cdeeca5a9a647ed5fae4
Nirzon Taru Karmakar <nirzonk47@gmail.com>
2023-06-18 13:44:33 +0530

    added solution for 53

---
+++ Random/LC_53.java
@@ -0,0 +1,21 @@
+public class LC_53 {
+
+    public static int maxSubArray(int[] nums) {
+        int sum = 0, maxSum = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            sum += num;
+            maxSum = Math.max(sum, maxSum);
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(maxSubArray(nums));
+    }
+}

fb8e6ff2415a173dae83394507bc68466c935cba
Nirzon Taru Karmakar <nirzonk47@gmail.com>
2023-07-06 16:33:09 +0530

    added solution for 709

---
+++ Random/LC_709.java
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/to-lower-case/
+
+public class LC_709 {
+    public static String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(toLowerCase("Hello"));
+    }
+}

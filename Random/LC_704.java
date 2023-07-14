922aa9d5950dfa0a96c7d5be7e6e0a9facf3949b
Nirzon Taru Karmakar <nirzonk47@gmail.com>
2023-07-14 19:02:27 +0530

    added solution for 704

---
+++ Random/LC_704.java
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/binary-search/
+
+public class LC_704 {
+    public static int search(int[] nums, int target) {
+        // Just normal binary search
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        int target = 9;
+
+        System.out.println(search(nums, target));
+    }
+}
